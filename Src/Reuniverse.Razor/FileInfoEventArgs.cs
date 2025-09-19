a82dea63d96eb3657e1636c35733b0449ce7bd58
Petr Pivoňka <petrpiv1@gmail.com>
2025-09-19 22:57:25 +0200

    Add MaxFileCount and MaxFileSize

---
+++ Src/Reuniverse.Razor/FileInfoEventArgs.cs
@@ -0,0 +1,3 @@
+﻿namespace Reuniverse.Razor;
+
+public sealed record FileInfoEventArgs(string FileName, int Size, DateTimeOffset LastModified);
--- Src/Reuniverse.Razor/ReuUploadArea.razor.js
+++ Src/Reuniverse.Razor/ReuUploadArea.razor.js
@@ -3,9 +3,27 @@ export function addHandlers(element, dotNetHelper) {
         if (event.target.files.length === 0) {
             return;
         }
-        const file = event.target.files[0];
-        const data = new Uint8Array(await file.arrayBuffer());
-        await dotNetHelper.invokeMethodAsync("UploadAsync", file.name, data, file.lastModified);
+
+        const maxFileCount = await dotNetHelper.invokeMethodAsync("GetMaxFileCountAsync");
+        const maxFileSize = await dotNetHelper.invokeMethodAsync("GetMaxFileSizeAsync");
+
+        const files = event.target.files;
+        const toUploadCount = maxFileCount == 0 ? files.length : Math.min(files.length, maxFileCount);
+
+        for (let i = 0; i < files.length; i++) {
+            const file = files[i];
+            if (i >= toUploadCount) {
+                await dotNetHelper.invokeMethodAsync("OnFileExceedCountAsync", file.name, file.size, file.lastModified);
+                continue;
+            }
+            if (maxFileSize > 0 && file.size > maxFileSize) {
+                await dotNetHelper.invokeMethodAsync("OnFileTooLargeAsync", file.name, file.size, file.lastModified);
+                continue;
+            }
+            const data = new Uint8Array(await file.arrayBuffer());
+            await dotNetHelper.invokeMethodAsync("UploadAsync", file.name, data, file.lastModified);
+        }
+
         event.target.value = null;
     });
 }
