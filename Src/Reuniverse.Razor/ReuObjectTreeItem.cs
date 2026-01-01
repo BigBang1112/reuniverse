dbdd400c6274b5d88285bcc4a46ebc3ac94b157e
Petr Pivoňka <petrpiv1@gmail.com>
2026-01-01 16:05:09 +0100

    Improve checkbox styling

--- Src/Reuniverse.Razor/ReuObjectTreeItem.cs
+++ Src/Reuniverse.Razor/ReuObjectTreeItem.cs
@@ -2,5 +2,37 @@
 
 public sealed record ReuObjectTreeItem(object Parent, int Index, object? Value)
 {
-    public override string ToString() => $"#{Index} {Value}";
+    public object? Value { get; private set; } = Value;
+
+    public override string ToString() => $"#{Index} {Value ?? "null"}";
+
+    public bool SetValue(object? newValue)
+    {
+        var parentType = Parent.GetType();
+        if (parentType.IsArray)
+        {
+            var array = (Array)Parent;
+            if (array.GetValue(Index)?.Equals(newValue) == true)
+            {
+                return false;
+            }
+            array.SetValue(newValue, Index);
+            Value = newValue;
+            return true;
+        }
+        else if (Parent is System.Collections.IList list)
+        {
+            if (list[Index]?.Equals(newValue) == true)
+            {
+                return false;
+            }
+            list[Index] = newValue;
+            Value = newValue;
+            return true;
+        }
+        else
+        {
+            throw new InvalidOperationException("Parent is not an array or a list.");
+        }
+    }
 }
