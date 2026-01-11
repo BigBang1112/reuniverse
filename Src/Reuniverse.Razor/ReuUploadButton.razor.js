c2b7df46e187a189db0264e24642fb74776344e5
Petr Pivoňka <petrpiv1@gmail.com>
2026-01-11 21:26:14 +0100

    Add ReuUploadButton

---
+++ Src/Reuniverse.Razor/ReuUploadButton.razor.js
@@ -0,0 +1,29 @@
+export function addHandlers(element, dotNetHelper) {
+    element.addEventListener("change", async (event) => {
+        if (event.target.files.length === 0) {
+            return;
+        }
+
+        const maxFileCount = await dotNetHelper.invokeMethodAsync("GetMaxFileCountAsync");
+        const maxFileSize = await dotNetHelper.invokeMethodAsync("GetMaxFileSizeAsync");
+
+        const files = event.target.files;
+        const toUploadCount = maxFileCount == 0 ? files.length : Math.min(files.length, maxFileCount);
+
+        for (let i = 0; i < files.length; i++) {
+            const file = files[i];
+            if (i >= toUploadCount) {
+                await dotNetHelper.invokeMethodAsync("OnFileExceedCountAsync", file.name, file.size, file.lastModified);
+                continue;
+            }
+            if (maxFileSize > 0 && file.size > maxFileSize) {
+                await dotNetHelper.invokeMethodAsync("OnFileTooLargeAsync", file.name, file.size, file.lastModified);
+                continue;
+            }
+            const data = new Uint8Array(await file.arrayBuffer());
+            await dotNetHelper.invokeMethodAsync("UploadAsync", file.name, data, file.lastModified);
+        }
+
+        event.target.value = null;
+    });
+}
