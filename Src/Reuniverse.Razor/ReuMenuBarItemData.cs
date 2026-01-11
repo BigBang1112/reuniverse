3becc061dd14cf2fa86caae253b458ffa8522dfe
Petr Pivoňka <petrpiv1@gmail.com>
2026-01-11 15:38:18 +0100

    Add ReuMenuBar

---
+++ Src/Reuniverse.Razor/ReuMenuBarItemData.cs
@@ -0,0 +1,10 @@
+﻿namespace Reuniverse.Razor;
+
+public sealed class ReuMenuBarItemData
+{
+    public required string Name { get; init; }
+    public string? Id { get; init; }
+    public List<ReuMenuBarItemData> Items { get; init; } = [];
+    public Func<Task>? Action { get; init; }
+    public string? Href { get; init; }
+}
