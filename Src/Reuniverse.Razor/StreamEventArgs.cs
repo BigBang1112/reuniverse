373eaca17c9b0c91c05406d336e03761a9acb626
Petr Pivoňka <petrpiv1@gmail.com>
2026-02-01 02:24:36 +0100

    Add streaming option for Uploaders

---
+++ Src/Reuniverse.Razor/StreamEventArgs.cs
@@ -0,0 +1,14 @@
+﻿namespace Reuniverse.Razor;
+
+public sealed record StreamEventArgs(string FileName, long Size, Stream Stream, DateTimeOffset LastModified) : IDisposable, IAsyncDisposable
+{
+    public void Dispose()
+    {
+        Stream.Dispose();
+    }
+
+    public ValueTask DisposeAsync()
+    {
+        return Stream.DisposeAsync();
+    }
+}
