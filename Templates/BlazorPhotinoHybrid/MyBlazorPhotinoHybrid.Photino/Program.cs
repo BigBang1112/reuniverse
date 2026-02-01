c6ea21dbdb4bbeeb629e78f10002131fe560607e
Petr Pivoňka <petrpiv1@gmail.com>
2026-02-01 17:04:17 +0100

    Add MyBlazorPhotinoHybrid template

---
+++ Templates/BlazorPhotinoHybrid/MyBlazorPhotinoHybrid.Photino/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Photino.Blazor;
+
+namespace MyBlazorPhotinoHybrid.Photino;
+
+internal class Program
+{
+    [STAThread]
+    static void Main(string[] args)
+    {
+        var builder = PhotinoBlazorAppBuilder.CreateDefault(args);
+        builder.Services.AddLogging();
+
+        // register root component
+        builder.RootComponents.Add<App>("app");
+
+        var app = builder.Build();
+
+        // customize window
+        app.MainWindow
+            .SetSize(1280, 720)
+            .SetIconFile("favicon.ico")
+            .SetTitle("MyBlazorPhotinoHybrid");
+
+        AppDomain.CurrentDomain.UnhandledException += (sender, error) =>
+        {
+            app.MainWindow.ShowMessage("Fatal exception", error.ExceptionObject.ToString());
+        };
+
+        app.Run();
+    }
+}
