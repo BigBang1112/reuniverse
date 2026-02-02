962b4184be85b27cc70766fd1627d57681602aa0
Petr Pivoňka <petrpiv1@gmail.com>
2026-02-02 21:17:57 +0100

    Fix streamed upload

--- Src/Reuniverse.Razor/ReuUploadButton.razor.js
+++ Src/Reuniverse.Razor/ReuUploadButton.razor.js
@@ -1,27 +1,29 @@
-export function addHandlers(element, dotNetHelper) {
+export function addHandlers(element, dotNetHelper, streamed) {
     element.addEventListener("change", async (event) => {
         if (event.target.files.length === 0) {
             return;
         }
 
-        const maxFileCount = await dotNetHelper.invokeMethodAsync("GetMaxFileCountAsync");
-        const maxFileSize = await dotNetHelper.invokeMethodAsync("GetMaxFileSizeAsync");
+        if (!streamed) {
+            const maxFileCount = await dotNetHelper.invokeMethodAsync("GetMaxFileCountAsync");
+            const maxFileSize = await dotNetHelper.invokeMethodAsync("GetMaxFileSizeAsync");
 
-        const files = event.target.files;
-        const toUploadCount = maxFileCount == 0 ? files.length : Math.min(files.length, maxFileCount);
+            const files = event.target.files;
+            const toUploadCount = maxFileCount == 0 ? files.length : Math.min(files.length, maxFileCount);
 
-        for (let i = 0; i < files.length; i++) {
-            const file = files[i];
-            if (i >= toUploadCount) {
-                await dotNetHelper.invokeMethodAsync("OnFileExceedCountAsync", file.name, file.size, file.lastModified);
-                continue;
+            for (let i = 0; i < files.length; i++) {
+                const file = files[i];
+                if (i >= toUploadCount) {
+                    await dotNetHelper.invokeMethodAsync("OnFileExceedCountAsync", file.name, file.size, file.lastModified);
+                    continue;
+                }
+                if (maxFileSize > 0 && file.size > maxFileSize) {
+                    await dotNetHelper.invokeMethodAsync("OnFileTooLargeAsync", file.name, file.size, file.lastModified);
+                    continue;
+                }
+                const data = new Uint8Array(await file.arrayBuffer());
+                await dotNetHelper.invokeMethodAsync("UploadAsync", file.name, data, file.lastModified);
             }
-            if (maxFileSize > 0 && file.size > maxFileSize) {
-                await dotNetHelper.invokeMethodAsync("OnFileTooLargeAsync", file.name, file.size, file.lastModified);
-                continue;
-            }
-            const data = new Uint8Array(await file.arrayBuffer());
-            await dotNetHelper.invokeMethodAsync("UploadAsync", file.name, data, file.lastModified);
         }
 
         event.target.value = null;
