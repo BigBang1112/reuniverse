d36869c2c082a07097bedb981c79fa97e92a4ea2
Petr Pivoňka <petrpiv1@gmail.com>
2026-04-05 19:49:42 +0200

    Add ByteHelper

---
+++ Src/Reuniverse.Razor/ByteHelper.cs
@@ -0,0 +1,19 @@
+﻿namespace Reuniverse.Razor;
+
+internal static class ByteHelper
+{
+    public static string ToByteSize(int size)
+    {
+        if (size < 1024)
+        {
+            return $"{size} B";
+        }
+
+        if (size < 1024 * 1024)
+        {
+            return $"{size / 1024.0:F2} kB";
+        }
+
+        return $"{size / (1024.0 * 1024.0):F2} MB";
+    }
+}
