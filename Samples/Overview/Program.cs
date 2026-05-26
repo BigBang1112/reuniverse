55c07dabb1acb1199c4795589e211c2e60b0903e
Petr Pivoňka <petrpiv1@gmail.com>
2026-05-26 22:25:17 +0200

    Add prerendering

--- Samples/Overview/Program.cs
+++ Samples/Overview/Program.cs
@@ -6,7 +6,12 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddScoped<EventService>();
+ConfigureServices(builder.Services, builder.HostEnvironment.BaseAddress);
 
 await builder.Build().RunAsync();
+
+static void ConfigureServices(IServiceCollection services, string baseAddress)
+{
+    services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(baseAddress) });
+    services.AddScoped<EventService>();
+}
